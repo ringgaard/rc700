10af17134fa671e337c9f8d5100444a29a5fa303
Michael Ringgaard <michael@ringgaard.com>
2014-06-10 21:14:36 +0200

    Code signing for Windows install executable

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ wine cmd /C winbuild.cmd
 
 echo "===== Make rc700-src.zip"
 rm -f /mnt/www/rc702/rc700-src.zip
-zip /mnt/www/rc702/rc700-src.zip *.c *.h Makefile Makefile.win roa375.rom rccpm22.imd
+zip /mnt/www/rc702/rc700-src.zip *.c *.h *.ico *.rc Makefile Makefile.win roa375.rom rccpm22.imd
 
 echo "===== Make rc700.zip"
 rm -f /mnt/www/rc702/rc700.zip
@@ -21,5 +21,9 @@ zip /mnt/www/rc702/rc700-win.zip rc700.exe rccpm22.imd SDL.dll
 
 echo "===== Make rc700-win-setup.exe"
 wine ../innosetup/ISCC rc700.iss /o.
-cp rc700-win-setup.exe /mnt/www/rc702/
+osslsigncode sign -pkcs12 ../certum.pfx -pass hemlig \
+    -n "RC700 Simulator" \
+    -i "http://www.jbox.dk/rc702/simulator.shtm" \
+    -in rc700-win-setup.exe \
+    -out /mnt/www/rc702/rc700-win-setup.exe
 
